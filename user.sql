dbeb405e5665f8fc8c2fd02715a38aab6b511a85
limiao <limiao@feinno.com>
2014-12-15 15:42:35 +0800

    添加数据创建脚本

---
+++ user.sql
@@ -0,0 +1,18 @@
+﻿
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for user
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(64) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `username` varchar(32) NOT NULL,
+  `portrait` varchar(128) NOT NULL,
+  `passwd` varchar(32) NOT NULL,
+  `createdTime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '创建时间',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`id`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=4077 DEFAULT CHARSET=utf8mb4;
